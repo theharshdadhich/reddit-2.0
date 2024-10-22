b43d60aead2420cdc83f09c029be864a24b5e248
Harsh Dadhich <harshdadhich1109@gmail.com>
2024-10-22 09:34:42 +0530

    API changes

--- src/components/homepage/GeneralFeed.tsx
+++ src/components/homepage/GeneralFeed.tsx
@@ -3,20 +3,26 @@ import PostFeed from '../PostFeed'
 import { INFINITE_SCROLL_PAGINATION_RESULTS } from '@/config'
 
 const GeneralFeed = async () => {
-  const posts = await db.post.findMany({
-    orderBy: {
-      createdAt: 'desc',
-    },
-    include: {
-      votes: true,
-      author: true,
-      comments: true,
-      subreddit: true,
-    },
-    take: INFINITE_SCROLL_PAGINATION_RESULTS, // 4 to demonstrate infinite scroll, should be higher in production
-  })
+  try {
+    const posts = await db.post.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        votes: true,
+        author: true,
+        comments: true,
+        subreddit: true,
+      },
+      take: INFINITE_SCROLL_PAGINATION_RESULTS, // 4 for demo, should be higher in production
+    })
 
-  return <PostFeed initialPosts={posts} />
+    return <PostFeed initialPosts={posts} />
+  } catch (error) {
+    console.error('Error fetching posts:', error)
+    // Handle the error gracefully
+    return <div>Unable to load posts. Please try again later.</div>
+  }
 }
 
 export default GeneralFeed
